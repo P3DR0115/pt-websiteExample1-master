f01c87acbc1dc52048edd876a7c39b4114022440
Pedro A. Tresgallo <P3dro115@gmail.com>
2018-09-11 16:54:46 -0500

    Add project files.

---
+++ pt-websiteExample1/helloWorld.js
@@ -0,0 +1,23 @@
+﻿// helloWorld.js
+function initialize()
+{
+    var buttonPressed = 0;
+    var greeting = 'Hello World!'
+
+    alert('Hello Professor!');
+
+    main();
+}
+
+function main()
+{
+
+}
+
+function doStuff()
+{
+    buttonPressed++;
+
+    alert(greeting + ' times pressed: ' + buttonPressed);
+    
+}
